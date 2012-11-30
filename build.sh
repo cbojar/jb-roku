5ac10b5a77e11a2f0e42bb7c9f6e05da76f7df24
chris@cbojar.net <chris@cbojar.net>
2012-11-30 12:42:42 -0500

    Added build script to build packages for developer deployment. This does not perform the necessary operations to deploy to the store.
    --HG--
    extra : transplant_source : %CB5%82%80a%A4%DB%B2%F3%D5%5E0x%D1%D6%FC.%A0%07I

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+TARGET="JB-Roku.zip"
+BRSALL="source/jupiterbroadcasting-roku.brs" # Monolithic source file to be made
+
+# Filter list of files only to those needing built
+FILES=$( find ./ ) # List all files
+FILES=$( echo "$FILES" | grep -v "$0" ) # Filter out this script
+FILES=$( echo "$FILES" | grep -v ".git" ) # Filter out Git-related files
+FILES=$( echo "$FILES" | grep -v ".hg" ) # Filter out Mercurial-related files
+FILES=$( echo "$FILES" | grep -v ".svn" ) # Filter out SVN-related files
+FILES=$( echo "$FILES" | grep -v ".project" ) # Filter out Eclipse project file
+FILES=$( echo "$FILES" | grep -v ".buildpath" ) # Filter out Eclipse buildpath file
+FILES=$( echo "$FILES" | grep -v "$TARGET" ) # Filter out target file
+FILES=$( echo "$FILES" | grep -v "^./$" ) # Filter out current directory listing
+
+# Concatenate all source files together into a single file to improve compression
+BRS=$( echo "$FILES" | grep ".brs$" )
+cat $BRS | tr -d "\t" | grep -v "^'" | grep -v "^$" > "$BRSALL"
+
+# Filter out source files
+FILESNOSRC=$( echo "$FILES" | grep -v ".brs$" | grep -v "source" )
+
+# Remove target file if it already exists
+if [ -e "$TARGET" ]; do
+	rm -f "$TARGET"
+done
+
+# Build compressed archive
+echo "$BRSALL" | zip -9 -@ "$TARGET" # Add in monolithic source file
+echo "$FILESNOSRC" | zip -9 -@ "$TARGET" # Add in all other resources
+
+rm "$BRSALL" # Remove monolithic source file
