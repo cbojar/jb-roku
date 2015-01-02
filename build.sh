5c6abd0075627e93e800afa80f18e52bab0db4d4
cbojar <chris@cbojar.net>
2015-01-02 12:43:59 -0500

    Clean up build script a bit, add option to specify output file.

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 DEBUG=0
 PRESERVE_TEMP=0
-TARGET="JB-Roku.zip"
+DEFAULT_TARGET="JB-Roku.zip"
 BRSALL="source/jupiterbroadcasting-roku.brs" # Monolithic source file to be made
 
-PARSED_OPTIONS=$( getopt -n "$0" -o hdp --long "help,debug,preserve-temp"  -- "$@" )
+target="$DEFAULT_TARGET"
+
+PARSED_OPTIONS=$( getopt -n "$0" -o hdpo: --long "help,debug,preserve-temp,output:"  -- "$@" )
 if [ $? -ne 0 ]; then #Bad arguments, something has gone wrong
 	exit 1
 fi
@@ -13,9 +15,13 @@ eval set -- "$PARSED_OPTIONS" # getopt magic
 while true; do
 	case "$1" in
 		-h|--help)
-			echo "Usage: $0 [-d|--debug] [-p|--preserve-temp]"
+			echo "Usage: $0 [[-d|--debug]|[-p|--preserve-temp]] [-o|--output <output-file>]"
 			shift
 			exit;;
+		-o|--ouput)
+			target="$2"
+			echo "File will be saved to \"$2\""
+			shift 2;;
 		-d|--debug)
 			DEBUG=1
 			shift;;
@@ -29,24 +35,21 @@ while true; do
 done
 
 # Filter list of files only to those needing built
-FILES=$( find ./ ) # List all files
-FILES=$( echo "$FILES" | grep -v "$0" ) # Filter out this script
-FILES=$( echo "$FILES" | grep -v '\.git' ) # Filter out Git-related files
-FILES=$( echo "$FILES" | grep -v '\.hg' ) # Filter out Mercurial-related files
-FILES=$( echo "$FILES" | grep -v '\.svn' ) # Filter out SVN-related files
-FILES=$( echo "$FILES" | grep -v '\.project' ) # Filter out Eclipse project file
-FILES=$( echo "$FILES" | grep -v '\.buildpath' ) # Filter out Eclipse buildpath file
-FILES=$( echo "$FILES" | grep -v '\.settings' ) # Filter out Eclipse settings folder
-FILES=$( echo "$FILES" | grep -v 'resources' ) # Filter out resources folder
-FILES=$( echo "$FILES" | grep -v "$TARGET" ) # Filter out target file
-FILES=$( echo "$FILES" | grep -v '^\./$' ) # Filter out current directory listing
+files=$( find . -type f ) # List all files
+files=$( egrep -v "$0" <<< "$files" ) # Filter out this script
+files=$( egrep -v '(\.git|\.hg|\.svn)' <<< "$files" ) # Filter out VCS-related files
+files=$( egrep -v '(\.project|\.buildpath|\.settings)' <<< "$files" ) # Filter out Eclipse-related files
+files=$( egrep -v 'resources' <<< "$files" ) # Filter out resources folder
+files=$( grep -v "$DEFAULT_TARGET" <<< "$files" ) # Filter out default target file
+files=$( grep -v "$target" <<< "$files" ) # Filter out target file
+
+# Remove target file if it already exists
+if [ -e "$target" ]; then
+	rm -f "$target"
+fi
 
 if [ $DEBUG = 1 ]; then # Don't concatenate all files if debugging
-	# Remove target file if it already exists
-	if [ -e "$TARGET" ]; then
-		rm -f "$TARGET"
-	fi
-	echo "$FILES" | zip -9 -@ "$TARGET" # Add in all resources
+	zip -9 -@ "$target" <<< "$files" # Add in all resources
 	exit
 fi
 
@@ -54,20 +57,17 @@ fi
 if [ -e "$BRSALL" ]; then
 	rm -f "$BRSALL"
 fi
-BRS=$( echo "$FILES" | grep ".brs$" )
-cat $BRS | tr -d "\t" | sed -r 's|^\s+||g' | sed -r 's|\s+$||g' | grep -v "^'" | grep -v "^REM " | grep -v "^REM$" | grep -v "^$" > "$BRSALL"
+brs=$( egrep  "\.brs$" <<< "$files" )
+
+# Remove tabs, leading spaces, trailing spaces, comments, and blank lines
+cat $brs | tr -d "\t" | sed -r 's|^\s+||g' | sed -r 's|\s+$||g' | grep -v "^'" | grep -v "^REM " | grep -v "^REM$" | grep -v "^$" > "$BRSALL"
 
 # Filter out source files
-FILESNOSRC=$( echo "$FILES" | grep -v ".brs$" | grep -v "source" )
-
-# Remove target file if it already exists
-if [ -e "$TARGET" ]; then
-	rm -f "$TARGET"
-fi
+filesnosrc=$( egrep -v "\.brs$" <<< "$files" | grep -v "source" )
 
 # Build compressed archive
-echo "$BRSALL" | zip -9 -@ "$TARGET" # Add in monolithic source file
-echo "$FILESNOSRC" | zip -9 -@ "$TARGET" # Add in all other resources
+zip -9 -@ "$target" <<< "$BRSALL" # Add in monolithic source file
+zip -9 -@ "$target" <<< "$filesnosrc" # Add in all other resources
 
 if [ $PRESERVE_TEMP = 0 ]; then
 	rm "$BRSALL" # Remove monolithic source file
